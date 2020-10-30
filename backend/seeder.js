f964566ede4eddcf1009d26d16899b938a350f4e
Nano Adam <nanoproductions2@gmail.com>
2020-10-30 18:11:04 -0400

    Working on seeder still

--- backend/seeder.js
+++ backend/seeder.js
@@ -1,3 +1,13 @@
 const { connectDB } = require('./config/db');
+const mongoose = require('mongoose');
+
+const creators = require('./data/creators');
 
 require('dotenv').config();
+
+connectDB();
+
+const importData = () => {
+  try {
+  } catch (err) {}
+};
